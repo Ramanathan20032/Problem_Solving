203d0458330012246ea3ee9d2dc3a1cf64e47f06
Ramanathan20032 <154827095+Ramanathan20032@users.noreply.github.com>
2024-03-15 14:48:53 +0530

    SystemOutPrintln is added

---
+++ website/SystemOutPrintln.java
@@ -0,0 +1,18 @@
+package theory;
+
+public class SystemOutPrintln {
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+	
+	/*
+	 * 
+	 * [class]System   	- this is final class from package[java.lang]  
+	 * 
+	 * [object]out		- this is a static variable inside the of System class & type of printStream  
+	 * 
+	 * [Method]print()	- this is a method inside the printStream class from package[java.io]
+	 * 					  printStream has a method - println
+	 * 
+	 */
+}
