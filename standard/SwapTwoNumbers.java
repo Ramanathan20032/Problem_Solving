ad4b31c576663f8aae041ab01262a9525f62bccf
Ramanathan20032 <154827095+Ramanathan20032@users.noreply.github.com>
2024-06-23 17:43:16 +0530

    Update SwapTwoNumbers.java

--- standard/SwapTwoNumbers.java
+++ standard/SwapTwoNumbers.java
@@ -1,25 +1,33 @@
 package Standard;
 
-public class SwapTwoNumbers {
+public class swapTwoNumbers {
     public static void main(String[] args) {
-        int a = 2;
-        int b = 3;
-        swap(a, b);
+        swap(2, 3);
+        System.out.println("-----------------");
+        swap1(4, 5);
+        System.out.println("-----------------");
+        swap2(6, 7);
     }
-
+    
     static void swap(int a, int b){
+        System.out.println("a : " + a + " " + "b: " + b);
         int temp = a;
         a = b;
         b = temp;
-        System.out.println("a : "+a);
-        System.out.println("b : "+b);
+        System.out.println("a : " + a + " " + "b: " + b);
     }
-
     static void swap1(int a, int b){
+        System.out.println("a : " + a + " " + "b: " + b);
         a = a + b;
         b = a - b;
         a = a - b;
-        System.out.println("a : "+a);
-        System.out.println("b : "+b);
+        System.out.println("a : " + a + " " + "b: " + b);
+    }
+    static void swap2(int a, int b){
+        System.out.println("a : " + a + " " + "b: " + b);
+        a = a * b;
+        b = a / b;
+        a = a / b;
+        System.out.println("a : " + a + " " + "b: " + b);
     }
 }
