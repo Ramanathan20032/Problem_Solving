f2dc09f601dd79643bd79171e44310adb183032b
Ramanathan20032 <154827095+Ramanathan20032@users.noreply.github.com>
2024-06-27 13:29:24 +0530

    Update ValueEqualToIndexValue.java

--- GfG/medium/ValueEqualToIndexValue.java
+++ GfG/medium/ValueEqualToIndexValue.java
@@ -5,15 +5,14 @@ package medium;
 // find the value which is equal to the index
 public class ValueEqualToIndexValue {
     public static void main(String[] args) {
-        int arr[] = {15, 2, 45, 12, 7};
-        System.out.println(logic(arr));
+        int[] arr = {15, 2, 34, 4, 22};
+        System.out.println(equalToIndex(arr));
     }
-
-    static int logic(int[] arr){
-        int result = 0;
+    static ArrayList<Integer> equalToIndex(int[] arr){
+        ArrayList<Integer> result = new ArrayList<>();
         for (int i = 0; i < arr.length; i++) {
-            if(arr[i] == i+1){
-                result = arr[i];
+            if(arr[i] == i + 1){
+                 result.add(arr[i]);
             }
         }
         return result;
